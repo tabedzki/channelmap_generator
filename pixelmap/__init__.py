a1a1e57f9bd3a34b9ce8b383403ab80f238bdac8
Maxime Beau <maximebeaujeanroch047@gmail.com>
2026-09-14 10:27:17 -0400

    patch version bump - v1.3.1
    new endpoint for health check

--- pixelmap/__init__.py
+++ pixelmap/__init__.py
@@ -7,6 +7,6 @@ Includes both a programmatic API and an interactive browser-based GUI.
 
 from .backend import *
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __author__ = "Maxime Beau"
 __email__ = "maxime@princeton.edu"
